40e26bf14dbebfe935c3c8b4a00aa917a5076355
علش <153824248+nesrx@users.noreply.github.com>
2024-01-05 11:32:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6783270820:AAHIHhTO4gGYx5OqZiasdZRUJRxeQnl-m5Y")
 
     APP_ID = int(os.environ.get("APP_ID", 16314924))
 
